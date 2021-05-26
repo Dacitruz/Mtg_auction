09880e688db74f66b00d8c77d42eeea1219da33d
Dacenko Daniel <32745938+Dacitruz@users.noreply.github.com>
2021-05-26 19:35:16 +0300

    Update settings.py

--- Auction/settings.py
+++ Auction/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydb',
         'USER': 'postgres',
-        'PASSWORD': 'prince228',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
